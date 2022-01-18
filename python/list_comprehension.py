9cb9e964aedbbcc285c473ba263daa82245af2bc
matjazbrilj <matjaz.brilj@editor.si>
2022-01-18 16:36:12 +0100

    [python][list_comprehension]: fix merge_and_sum_by_id

--- python/list_comprehension.py
+++ python/list_comprehension.py
@@ -15,16 +15,35 @@ def merge_and_sum_by_id(data, unique_id_name, sum_key_names):
     new_data = []
     current_id = None
     tmp = {}
+    total_singles = 0
     for index, line in enumerate(data):       
         id = line[unique_id_name]
 
-        if current_id != id or index >= len(data)-1:    
+        if current_id != id:    
             if index > 0:
                 new_data.append(tmp)
             current_id = id
             tmp = line
+            total_singles = 0
         
-        for k in sum_key_names:
-            tmp[k] += line[k]
+        total_singles += 1
+        if total_singles > 1:
+            for k in sum_key_names:
+                tmp[k] += line[k]
 
+    new_data.append(tmp)
     return new_data
+
+
+test_data = [
+    {'my_id': 1, 'name': 'test', 'num1': 5, 'num2': 10},
+    {'my_id': 1, 'name': 'test2', 'num1': 2, 'num2': 3},
+    {'my_id': 1, 'name': 'test3', 'num1': 1, 'num2': 2},
+    {'my_id': 6, 'name': 'test4', 'num1': 4, 'num2': 5}
+]
+
+result = merge_and_sum_by_id(test_data, 'my_id', ['num1', 'num2'])
+print("\n\n++++++++RESULTS++++++++")
+for line in result:
+    print(line)
+    
