0d62f7c26b561a8e652b529e22a5dd29575bf94b
Matjaž Brilj <brilj.matjaz@gmail.com>
2022-03-08 02:53:50 +0100

    [ADD]export csv

--- tests/csv_export_test.py
+++ tests/csv_export_test.py
@@ -10,19 +10,16 @@ from csv_import_export import export_csv_data
 class CSVImportTest(unittest.TestCase):
 
     def setUp(self):
-        self.file_name = 'test_data.csv'
-        self.path = os.path.join(os.getcwd(), 'data')
+        self.path = os.path.join(os.getcwd(), 'data', 'test_data.csv')
         self.object_test_data = [
-            {'product_name': 'Eldon Base for stackable storage shelf, platinum', 'contact_name': 'Muhammed MacIntyre', 'group_ids': ['1', '3', '4']},
-            {'product_name': '1.7 Cubic Foot Compact "Cube" Office Refrigerators', 'contact_name': False, 'group_ids': ['556', '293', '434', '412']},
-            {'product_name': 'Cardinal Slant-D Ring Binder, Heavy Gauge Vinyl', 'contact_name': 'Barry French', 'group_ids': '293'},
-            {'product_name': 'R380', 'contact_name': 'Clay Rozendal', 'group_ids': '483'}
+            {'product_name': 'Eldon Base for stackable storage shelf, platinum', 'company_ids': ['0', '1', '5'], 'contact_name': 'Muhammed MacIntyre', 'group_ids': ['1', '3', '4']},
+            {'product_name': '1.7 Cubic Foot Compact "Cube" Office Refrigerators', 'company_ids': ['0', '1'], 'contact_name': False, 'group_ids': ['556', '293', '434', '412']},
+            {'product_name': 'Cardinal Slant-D Ring Binder, Heavy Gauge Vinyl', 'company_ids': ['0', '1'], 'contact_name': 'Barry French', 'group_ids': '293'},
+            {'product_name': 'R380', 'company_ids': ['0', '1', '3'], 'contact_name': 'Clay Rozendal', 'group_ids': '483'}
         ]
         
-
     def test_object_to_csv(self):
         result = export_csv_data(
-            file_name = self.file_name,
             path = self.path,
             object_data_list = self.object_test_data
         )
--- tests/object_to_table_test.py
+++ tests/object_to_table_test.py
@@ -10,28 +10,32 @@ from src.lists import object_to_table
 class ObjectToTableTest(unittest.TestCase):
 
     def setUp(self):
-        self.object_list = [
-            {'product_name': 'Eldon Base for stackable storage shelf, platinum', 'contact_name': 'Muhammed MacIntyre', 'group_ids': ['1', '3', '4']},
-            {'product_name': '1.7 Cubic Foot Compact "Cube" Office Refrigerators', 'contact_name': False, 'group_ids': ['556', '293', '434', '412']},
-            {'product_name': 'Cardinal Slant-D Ring Binder, Heavy Gauge Vinyl', 'contact_name': 'Barry French', 'group_ids': '293'},
-            {'product_name': 'R380', 'contact_name': 'Clay Rozendal', 'group_ids': '483'}
+        self.object_data_list = [
+            {'product_name': 'Eldon Base for stackable storage shelf, platinum', 'company_ids': ['0', '1', '5', '10'], 'contact_name': 'Muhammed MacIntyre', 'group_ids': ['1', '3', '4']},
+            {'product_name': '1.7 Cubic Foot Compact "Cube" Office Refrigerators', 'company_ids': ['0', '1'], 'contact_name': False, 'group_ids': ['556', '293', '434', '412']},
+            {'product_name': 'Cardinal Slant-D Ring Binder, Heavy Gauge Vinyl', 'company_ids': ['0', '1'], 'contact_name': 'Barry French', 'group_ids': '293'},
+            {'product_name': 'R380', 'company_ids': ['0', '1', '3'], 'contact_name': 'Clay Rozendal', 'group_ids': '483'}
         ]
         self.expected_list = [
-            ['product_name', 'contact_name', 'group_ids'],
-            ['Eldon Base for stackable storage shelf, platinum', 'Muhammed MacIntyre', '1'],
-            ['', '', '3'],
-            ['', '', '4'],
-            ['1.7 Cubic Foot Compact "Cube" Office Refrigerators', '', '556'],
-            ['', '', '293'],
-            ['', '', '434'],
-            ['', '', '412'],
-            ['Cardinal Slant-D Ring Binder, Heavy Gauge Vinyl', 'Barry French', '293'],
-            ['R380', 'Clay Rozendal', '483'],
+            ['product_name', 'company_ids', 'contact_name', 'group_ids'],
+            ['Eldon Base for stackable storage shelf, platinum', '0', 'Muhammed MacIntyre', '1'],
+            ['', '1', '', '3'],
+            ['', '5', '', '4'],
+            ['', '10', '', ''],
+            ['1.7 Cubic Foot Compact "Cube" Office Refrigerators', '0', '', '556'],
+            ['', '1', '', '293'],
+            ['', '', '', '434'],
+            ['', '', '', '412'],
+            ['Cardinal Slant-D Ring Binder, Heavy Gauge Vinyl', '0', 'Barry French', '293'],
+            ['', '1', '', ''],
+            ['R380', '0', 'Clay Rozendal', '483'],
+            ['', '1', '', ''],
+            ['', '3', '', ''],
         ]        
 
     def test_object_to_csv(self):
         result = object_to_table(
-            object_list = self.object_list
+            object_list = self.object_data_list
         )
         self.assertListEqual(self.expected_list, result)
         
